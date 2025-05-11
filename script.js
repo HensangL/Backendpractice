9c2bc1b15271cf84381cf3396bc3dfc6237268b1
HensangL <lamahensang100@gmail.com>
2025-05-11 22:24:38 +0545

    5/11/2025

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const fs = require('node:fs');
-
-fs.appendFile("hello.txt","mynigga", function(err){
-    if(err) console.error(err); 
-    else console.log("done");
+const http= require('http');
+const server=http.createServer(function(req,res){
+ res.end("hello")
 })
-fs.rename("hello.txt","dhankuta.txt",function(err){
-    if(err) console.error("youtube");
-    else console.log("china")
-})
-fs.copyFile("dhankuta.txt","./copy/copy.txt",function(err){
-    if(err) console.error(err); 
-    else console.log("done"); 
-})
+server.listen(3000);
