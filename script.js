30f24aeeff00bba0e524997b77ee1c01afbac6ce
HensangL <lamahensang100@gmail.com>
2025-05-24 23:21:55 +0545

    ..

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app =express()
-app.get('/',function (req,res){
-    res.send('Helloworld')
+
+app.get("/",function(req,res ,next){
+return next(new Error("something borke again"))
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
-app.get('/profile',function(req,res){
-    res.send('NBA yb')
-})
+    
+
+
+
+
 app.listen(2828);
